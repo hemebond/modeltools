9a789408b81c85bbe8d80daced3b8c862d7d6354
James O'Neill <hemebond@gmail.com>
2025-05-17 22:09:33 +0900

    Update usage text to include the origin parameters.

--- makesp2.c
+++ makesp2.c
@@ -20,12 +20,13 @@
 
 void printusage(void)
 {
-	fprintf(stderr,
-"usage: makesp2 <sp2filename> <imageprintf> <numframes> <width> <height>\n"
-"example:\n"
-"makesp2 models/explosion1.sp2 models/explosion1_%%02d.tga 20 64 64\n"
-"(note: that would be frames named explosion1_00.tga then 01 and so on,\n"
-"       use %%d by itself for _0 then _1 and so on)\n"
+	(void)fprintf(
+		stderr,
+		"usage: makesp2 <sp2filename> <imageprintf> <numframes> <width> <height> [<origin_x> <origin_y>]\n"
+		"example:\n"
+		"makesp2 models/explosion1.sp2 models/explosion1_%%02d.tga 20 64 64\n"
+		"(note: that would be frames named explosion1_00.tga then 01 and so on,\n"
+		"       use %%d by itself for _0 then _1 and so on)\n"
 	);
 	exit(1);
 }
